7a46482870dadedfe97371bfbd0fe7138e6ac5f4
CodeKnight314 <tang.ri@northeastern.edu>
2024-11-08 15:51:50 +0000

    reword args

--- train.py
+++ train.py
@@ -7,20 +7,20 @@ from dataset import SyntheticNeRF
 from vol_render import vol_render
 
 def train_nerf(args):
-    dataset = SyntheticNeRF(args.dataset_path, mode="train", t_near=args.t_near, t_far=args.t_far, num_steps=args.num_steps)
-    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+    dataset = SyntheticNeRF(args.root, mode="train", t_near=args.t_near, t_far=args.t_far, num_steps=args.num_steps, size=args.size)
+    dataloader = DataLoader(dataset, batch_size=args.batch, shuffle=True)
 
     model = NeRF(pos_encoding_L=args.pos_encoding_L, dir_encoding_L=args.dir_encoding_L)
     model.train()
 
-    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
     photometric_loss = PhotometricLoss()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
 
-    for epoch in range(args.num_epochs):
+    for epoch in range(args.epochs):
         total_loss = 0.0
         for batch_idx, data in enumerate(dataloader):
             # Load data onto device
@@ -50,21 +50,21 @@ def train_nerf(args):
         avg_loss = total_loss / len(dataloader)
         print(f"Epoch {epoch + 1}/{args.num_epochs}, Loss: {avg_loss:.4f}")
 
-    # Save model checkpoint
     torch.save(model.state_dict(), args.save_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train a NeRF model")
-    parser.add_argument('--dataset_path', type=str, required=True, help='Path to dataset')
-    parser.add_argument('--batch_size', type=int, default=1, help='Batch size for training')
-    parser.add_argument('--learning_rate', type=float, default=5e-4, help='Learning rate for the optimizer')
-    parser.add_argument('--num_epochs', type=int, default=10, help='Number of epochs to train the model')
-    parser.add_argument('--save_path', type=str, default='nerf_model.pth', help='Path to save the trained model')
+    parser.add_argument('--root', type=str, required=True, help='Path to dataset')
+    parser.add_argument('--batch', type=int, default=1, help='Batch size for training')
+    parser.add_argument('--lr', type=float, default=5e-4, help='Learning rate for the optimizer')
+    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train the model')
+    parser.add_argument('--save', type=str, default='nerf_model.pth', help='Path to save the trained model')
     parser.add_argument('--pos_encoding_L', type=int, default=10, help='Number of frequencies for positional encoding')
     parser.add_argument('--dir_encoding_L', type=int, default=4, help='Number of frequencies for directional encoding')
-    parser.add_argument('--t_near', type=int, default=1, help='Near plane for ray sampling')
-    parser.add_argument('--t_far', type=int, default=128, help='Far plane for ray sampling')
+    parser.add_argument('--t_near', type=int, default=2, help='Near plane for ray sampling')
+    parser.add_argument('--t_far', type=int, default=6, help='Far plane for ray sampling')
     parser.add_argument('--num_steps', type=int, default=64, help='Number of steps for ray sampling')
+    parser.add_argument('--size', type=int, default=100, help='Dimension of image for NeRF to sample rays from')
     args = parser.parse_args()
 
     train_nerf(args)
